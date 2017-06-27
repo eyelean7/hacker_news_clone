dc2df0a199e57cd20e70a87c718bee119f3d58e8
Ilene Gorski and Katie Borel <ilene+katie@>
2017-06-27 14:52:48 -0700

    add new user functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,21 @@ class PostsController < ApplicationController
     @posts = Post.all
     render :index
   end
-
+  def new
+    @user = User.find(params[:user_id])
+    @post = Post.new
+  end
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+private
+  def post_params
+    params.require(:post).permit(:title, :url)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627160040) do
+ActiveRecord::Schema.define(version: 20170627210735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170627160040) do
     t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
